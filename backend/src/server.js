477c0012c3b3fdbe532979aeb6defdac31a003ba
Raphael2790 <rssshev@gmail.com>
2020-04-24 22:00:34 -0300

    Realizando a conexão com socketio entre o front e backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,33 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 const path = require('path');
 
+const http = require('http');
+const socketio = require('socket.io');
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 
 mongoose.connect(`mongodb+srv://${process.env.DATA_BASE}:${process.env.DATABASE_PASSWORD}@cluster0-zf6t1.gcp.mongodb.net/omnistack9?retryWrites=true&w=majority`,{
   useNewUrlParser:true,
   useUnifiedTopology:true,
+})
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id
+})
+
+app.use((req,res,next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
 })
 
 app.use(cors())
@@ -17,6 +39,6 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
-app.listen(3333, () =>{
+server.listen(3333, () =>{
   console.log("Servidor rodando...")
 })
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState}from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 
 import './styles.css'
 
@@ -7,6 +8,19 @@ import api from '../../services/api';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const user_id = localStorage.getItem('user');
+    const socket = socketio('http://localhost:3333', {
+      query:{
+        user_id
+      }
+    });
+    
+    socket.on('booking_request', data =>{
+      console.log(data);
+    })
+  }, [])
 
   useEffect(() => {
     async function loadSpots() {
